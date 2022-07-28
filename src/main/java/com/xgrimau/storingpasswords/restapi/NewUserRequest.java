bd6e7234f13d34184f9decced5ca4a3ca4fd81de
Xavi Grimau <x.grimauu@gmail.com>
2022-07-29 00:24:05 +0200

    clean up imports

--- src/main/java/com/xgrimau/storingpasswords/restapi/NewUserRequest.java
+++ src/main/java/com/xgrimau/storingpasswords/restapi/NewUserRequest.java
@@ -1,10 +1,5 @@
 package com.xgrimau.storingpasswords.restapi;
 
-import lombok.Data;
-import lombok.Setter;
-import org.springframework.lang.NonNull;
-
-
 public class NewUserRequest {
 
     public String username;
--- src/main/java/com/xgrimau/storingpasswords/service/UserService.java
+++ src/main/java/com/xgrimau/storingpasswords/service/UserService.java
@@ -9,12 +9,9 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.util.Base64;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
